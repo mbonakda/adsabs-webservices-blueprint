f566993112572ade5b457227e64b4787cb4539f1
Vladimir Sudilovsky <vsudilovsky@gmail.com>
2015-05-19 13:08:45 +0000

    management: Small refactor; add toplevel docstring

--- sample_application/manage.py
+++ sample_application/manage.py
@@ -1,7 +1,16 @@
+"""
+Example manage.py script, which is responsible for providing a command-line
+interface to application specific tasks, such as managing databases.
+"""
+
 from flask.ext.script import Manager
 from flask.ext.migrate import Migrate, MigrateCommand
 from models import db
 from app import create_app
+
+app = create_app()
+migrate = Migrate(app, db)
+manager = Manager(app)
 
 class CreateDatabase(Command):
     """
@@ -12,16 +21,6 @@ class CreateDatabase(Command):
         with create_app().app_context():
             db.create_all()
 
-app_ = create_app()
-
-app_.config.from_pyfile('config.py')
-try:
-  app_.config.from_pyfile('local_config.py')
-except IOError:
-  pass
-
-migrate = Migrate(app_, db)
-manager = Manager(app_)
 
 manager.add_command('db', MigrateCommand)
 manager.add_command('createdb', CreateDatabase())
